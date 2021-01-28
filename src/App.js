eefa340166252be91d1e7701bf5fb4bc12b3a310
ryanjeffrey <ryanjeffreysmith@gmail.com>
2021-01-27 18:00:59 -0600

    Added video background and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import ink from './ink.mp4';
+import neon from './neon.mp4';
+import red from './red.mp4';
+import tv from './tv.mp4';
+import underwater from './underwater.mp4';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+     <video 
+        autoPlay 
+        loop 
+        muted
+        style={{
+          position: 'absolute',
+          width: '100%',
+          left: '50%',
+          top: '50%',
+          height: '100%',
+          objectFit: 'cover',
+          transform: 'translate(-50%, -50%)',
+          zIndex: '-1'
+        }}
         >
-          Learn React
-        </a>
-      </header>
+       <source src={underwater} type ='video/mp4' />
+     </video>
+     <h1>fall classic</h1>
+     <i class="fas fa-bullhorn"></i>
+     <h2>END WHITE SUPREMACY</h2>
+     <i class="fas fa-fist-raised"></i>
+     <h2>BLACK LIVES MATTER</h2>
+     <i class="fas fa-equals"></i>
+     <h2>WE ARE ALL EQUALS</h2>
     </div>
   );
 }
